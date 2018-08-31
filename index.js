c4a2a990a2177062807d1315dba45868541689cc
overy1234 <overy1234@naver.com>
2018-08-31 14:26:19 +0900

    3

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express'); // 설치한 express module을 불러와서 변수(express)에 담습니다.
 var app = express(); //express를 실행하여 app object를 초기화 합니다.
 
-app.get('/',function(req,res){   // '/'
-    res.send('Hello World');  //"Hello World!"를 보냅니다.
+app.set("view engine","ejs");
+app.use(express.static(__dirname + '/public'));
+
+app.get('/hello',function(req,res){   // '/'
+    res.render("hello",{name:req.query.nameQuery});
+});
+
+app.get("/hello/:nameParam",function(req,res){
+    res.render("hello",{name:req.params.nameParam});
 });
 
 app.listen(3000,function(){  //3000번 포트를 사용합니다.
